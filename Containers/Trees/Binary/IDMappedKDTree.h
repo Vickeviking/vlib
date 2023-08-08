553b1291e0adae622b845d747ef54f3af2122e91
Vickeviking <92083048+Vickeviking@users.noreply.github.com>
2023-08-08 22:28:32 +0200

    changes done to IDMappedTree

--- Containers/Trees/Binary/IDMappedKDTree.h
+++ Containers/Trees/Binary/IDMappedKDTree.h
@@ -14,12 +14,12 @@ namespace VLIB{
 
 
 // Node of a KD Tree
- template <class T, std::size_t K>
+ template <class CoordType, std::size_t KDimensions>
     struct KDNode {
-    std::array<T, K> point;      // Coordinates of the point in K-dimensional space
+    std::array<CoordType, KDimensions> point;      // Coordinates of the point in K-dimensional space
     std::uint64_t uniqueID;      // Unique ID associated with the point
-    KDNode<T, K>* left;          // Pointer to the left child node
-    KDNode<T, K>* right;         // Pointer to the right child node
+    KDNode<CoordType, KDimensions>* left;          // Pointer to the left child node
+    KDNode<CoordType, KDimensions>* right;         // Pointer to the right child node
 
     // Private function to generate a unique ID
     static std::uint64_t generateUniqueID() {
@@ -28,81 +28,88 @@ namespace VLIB{
     };
 
     // Constructor with unique ID generation
-    KDNode(const std::array<T, K>& p) : point(p), 
+    KDNode(const std::array<CoordType, KDimensions>& p) : point(p), 
     uniqueID(generateUniqueID()), left(nullptr), right(nullptr) {}
     };
 
 
 // KD Tree that stores points in K-dimensional space
-template <class T, std::size_t K, class UserData>
+template <class CoordType, std::size_t KDimensions, class UserData>
 class IDMappedKDTree {
 private:
-    KDNode<T,K>* root;
-    std::unordered_map<std::uint64_t, UserData> userMap; // Map to store user-defined data
-    
+    KDNode<CoordType, KDimensions>* root;
+    std::unordered_map<std::uint64_t, std::tuple<UserData, std::array<CoordType, KDimensions>>> dataMap;
+
     // Helper functions
     std::size_t getTreeDepth() const;
 
 public:
-
     // constructor & destructor
     IDMappedKDTree() : root(nullptr) {}
     ~IDMappedKDTree() { delete root; }
 
     // Manage the tree
-    std::uint64_t insert(const std::array<T, K>& point, const UserData& userData); //returns handles to the unique ID
+    std::uint64_t insert(const std::array<CoordType, KDimensions>& point, const UserData& userData);
     void remove(std::uint64_t uniqueID);
     void balance();
     void clear();
     std::size_t size() const;
-    // search
-    std::uint64_t nearestNeighbor(const std::array<T, K>& point) const;
-    std::vector<std::uint64_t> rangeSearch(const std::array<T, K>& point, T distance) const;
+    std::uint64_t nearestNeighbor(const std::array<CoordType, KDimensions>& point) const;
+    std::vector<std::uint64_t> nearestNeighborsWithinRange(const std::array<CoordType, KDimensions>& point, CoordType maxDistance) const;
+    std::vector<std::uint64_t> rangeSearch(const std::array<CoordType, KDimensions>& point, CoordType distance) const;
     bool contains(std::uint64_t uniqueID) const;
 
-
     // Manage user-defined data associated with a point & KD-tree synchronization
-
-    void setPointCoordinates(std::uint64_t uniqueID, const std::array<T, K>& newCoordinates);
+    void setPointCoordinates(std::uint64_t uniqueID, const std::array<CoordType, KDimensions>& newCoordinates);
     void setUserData(std::uint64_t uniqueID, const UserData& userData);
-    
     const UserData& getUserData(std::uint64_t uniqueID) const;
-    const std::array<T, K>& getPointCoordinates(std::uint64_t uniqueID) const;
-
+    const std::array<CoordType, KDimensions>& getCoordinates(std::uint64_t uniqueID) const;
 };
 
 // Manage the tree
-
-template <class T, std::size_t K, class UserData>
-std::uint64_t IDMappedKDTree<T, K, UserData>::insert(const std::array<T, K>& point, const UserData& userData) {
+template <class CoordType, std::size_t KDimensions, class UserData>
+std::uint64_t IDMappedKDTree<CoordType, KDimensions, UserData>::insert(const std::array<CoordType, KDimensions>& point, const UserData& userData) {
     // Insert point into the KD-tree
-    // Generate a unique ID for the point
-    std::uint64_t uniqueID = KDNode<T, K>::generateUniqueID();
-    // Insert the user data into the hashmap using the unique ID as the key
-    userMap[uniqueID] = userData;
+    std::uint64_t uniqueID = KDNode<CoordType, KDimensions>::generateUniqueID();
+    dataMap[uniqueID] = std::make_tuple(userData, point);
     // ... continue with inserting the point into the KD-tree ...
-
-    // return the ID
     return uniqueID;
+
+    //TODO far from complete, be careful doing this function, check the book examples from drozdek data structures and algorithms in c++
 }
 
-
-// Manage the user-defined data associated with a point
-
-template <class T, std::size_t K, class UserData>
-void IDMappedKDTree<T, K, UserData>::setPointCoordinates(std::uint64_t uniqueID, const std::array<T, K>& newCoordinates) {
-    auto it = userMap.find(uniqueID);
-    if (it != userMap.end()) {
-        // Update the coordinates within the user data structure
-        it->second.setCoordinates(newCoordinates); // Assuming UserData has a setCoordinates method
-
-        // Optionally, update the KD-tree internal structure
-        // This step is not strictly necessary, but it's an option to keep the tree synchronized
-        // with the user data changes. It depends on how you want to manage this synchronization.
-        // If you choose to update the tree here, you'll need to implement a mechanism to
-        // find the node associated with the uniqueID and update its coordinates.
+// Manage user-defined data associated with a point & KD-tree synchronization
+template <class CoordType, std::size_t KDimensions, class UserData>
+void IDMappedKDTree<CoordType, KDimensions, UserData>::setPointCoordinates(std::uint64_t uniqueID, const std::array<CoordType, KDimensions>& newCoordinates) {
+    auto it = dataMap.find(uniqueID);
+    if (it != dataMap.end()) {
+        std::get<1>(it->second) = newCoordinates; // Update coordinates in the tuple
+        // Optionally, update the KD-tree internal structure if needed
+        // TODO:  UPDATE KD-TREE INTERNAL STRUCTURE HERE
     }
 }
+
+template <class CoordType, std::size_t KDimensions, class UserData>
+void IDMappedKDTree<CoordType, KDimensions, UserData>::setUserData(std::uint64_t uniqueID, const UserData& userData) {
+    auto it = dataMap.find(uniqueID);
+    if (it != dataMap.end()) {
+        std::get<0>(it->second) = userData; // Update user data in the tuple
+    }
+}
+
+
+// Retrieve user data using unique ID
+template <class CoordType, std::size_t KDimensions, class UserData>
+const UserData& IDMappedKDTree<CoordType, KDimensions, UserData>::getUserData(std::uint64_t uniqueID) const {
+    return std::get<0>(dataMap.at(uniqueID));
+}
+
+// Retrieve coordinates using unique ID
+template <class CoordType, std::size_t KDimensions, class UserData>
+const std::array<CoordType, KDimensions>& IDMappedKDTree<CoordType, KDimensions, UserData>::getCoordinates(std::uint64_t uniqueID) const {
+    return std::get<1>(dataMap.at(uniqueID));
+}
+
 
 
 }
